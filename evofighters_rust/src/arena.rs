efe2cc6b723deb73d56e85418bc78772bca61157
Josh Kuhn <deontologician@gmail.com>
2015-01-29 22:54:02 -0800

    More of do_round

--- evofighters_rust/src/arena.rs
+++ evofighters_rust/src/arena.rs
@@ -55,13 +55,6 @@ pub fn encounter(p1: &mut Creature, p2: &mut Creature, rng:
     children
 }
 
-enum Act {
-    Attacking = 1,
-    Defending,
-    Mating,
-    Other,
-}
-
 struct Chances {
     mate_chance: usize,
     p1_chance_to_hit: usize,
@@ -94,41 +87,42 @@ impl Chances {
     }
 }
 
-fn damage_matrix(p1_act: Act, p2_act: Act) -> Chances {
+fn damage_matrix(p1_act: eval::PerformableAction,
+                 p2_act: eval::PerformableAction) -> Chances {
+    use eval::PerformableAction as PA;
     match (p1_act, p2_act) {
-        (Act::Attacking, Act::Attacking) =>
-            Chances::new(  0, 75, 75, 50, 50,  0,  6),
-        (Act::Attacking, Act::Defending) =>
-            Chances::new(  0, 25, 25, 25, 25,  0,  0),
-        (Act::Attacking, Act::Mating   ) =>
-            Chances::new( 50, 50,  0, 75,  0, 70, 30),
-        (Act::Attacking, Act::Other    ) =>
-            Chances::new(  0,100,  0,100,  0,  0,  0),
-        (Act::Defending, Act::Defending) =>
-            Chances::new(  0,  0,  0,  0,  0,  0,  0),
-        (Act::Defending, Act::Mating   ) =>
-            Chances::new( 25,  0,  0,  0,  0, 70, 30),
-        (Act::Defending, Act::Other    ) =>
-            Chances::new(  0,  0,  0,  0,  0,  0,  0),
-        (Act::Mating, Act::   Mating   ) =>
-            Chances::new(100,  0,  0,  0,  0, 50, 50),
-        (Act::Mating, Act::   Other    ) =>
-            Chances::new( 75,  0,  0,  0,  0,  0,100),
-        (Act::Other, Act::    Other    ) =>
-            Chances::new(  0,  0,  0,  0,  0,  0,  0),
-        // the rest are duplicates of the above with swapped order
-        (Act::Defending, Act::Attacking) =>
-            Chances::new(  0, 25, 25, 25, 25,  0,  0),
-        (Act::Mating, Act::   Attacking) =>
-            Chances::new( 50,  0, 50,  0, 75, 30, 70),
-        (Act::Mating, Act::   Defending) =>
-            Chances::new( 25,  0,  0,  0,  0, 30, 70),
-        (Act::Other, Act::    Attacking) =>
-            Chances::new(  0,  0,100,  0,100,  0,  0),
-        (Act::Other, Act::    Defending) =>
-            Chances::new(  0,  0,  0,  0,  0,  0,  0),
-        (Act::Other, Act::    Mating   ) =>
-            Chances::new( 75,  0,  0,  0,  0,100,  0),
+        (PA::Attack(..), PA::Attack(..)) =>
+                               Chances::new(  0, 75, 75, 50, 50,  0,  6),
+        (PA::Attack(..), PA::Defend(..)) =>
+                               Chances::new(  0, 25, 25, 25, 25,  0,  0),
+        (PA::Attack(..),       PA::Mate) =>
+                               Chances::new( 50, 50,  0, 75,  0, 70, 30),
+        (PA::Attack(..),              _) =>
+                               Chances::new(  0,100,  0,100,  0,  0,  0),
+        (PA::Defend(..), PA::Defend(..)) =>
+                               Chances::new(  0,  0,  0,  0,  0,  0,  0),
+        (PA::Defend(..),       PA::Mate) =>
+                               Chances::new( 25,  0,  0,  0,  0, 70, 30),
+        (PA::Defend(..), PA::Attack(..)) =>
+                               Chances::new(  0, 25, 25, 25, 25,  0,  0),
+        (PA::Defend(..),              _) =>
+                               Chances::new(  0,  0,  0,  0,  0,  0,  0),
+        (PA::Mate,             PA::Mate) =>
+                               Chances::new(100,  0,  0,  0,  0, 50, 50),
+        (PA::Mate,       PA::Attack(..)) =>
+                               Chances::new( 50,  0, 50,  0, 75, 30, 70),
+        (PA::Mate,       PA::Defend(..)) =>
+                               Chances::new( 25,  0,  0,  0,  0, 30, 70),
+        (PA::Mate,                    _) =>
+                               Chances::new( 75,  0,  0,  0,  0,  0,100),
+        (_,              PA::Attack(..)) =>
+                               Chances::new(  0,  0,100,  0,100,  0,  0),
+        (_,              PA::Defend(..)) =>
+                               Chances::new(  0,  0,  0,  0,  0,  0,  0),
+        (_,                    PA::Mate) =>
+                               Chances::new( 75,  0,  0,  0,  0,100,  0),
+        (_,                           _) =>
+                               Chances::new(  0,  0,  0,  0,  0,  0,  0),
     }
 }
 
@@ -137,6 +131,13 @@ fn do_round(p1: &mut Creature,
             p2: &mut Creature,
             p2_act: eval::PerformableAction,
             rng: &mut ThreadRng) -> Option<Creature> {
-
+    let chances = damage_matrix(p1_act, p2_act);
+    fn damage_fun(chance: usize, mult: usize, rng: &mut ThreadRng) -> usize {
+        if rng.gen_range(0, 100) <= chance {
+            rng.gen_range(1, ((mult as f64/100.0) * 6.0) as usize)
+        } else {
+            0
+        }
+    }
     panic!("Oh noes")
 }
